fa97f7063cb5bea1898b508dff326055c5372028
Victor Mario Bertini <victor.m.bertini@gmail.com>
2022-02-10 00:36:59 -0300

    11

--- china.py
+++ china.py
@@ -12,4 +12,4 @@ subprocess.run('pip3 install requests', shell = True)
 
 subprocess.run('pip3 install packaging', shell = True)
 
-subprocess.run('printf "1\nhttps://drive.google.com/file/d/1ITGfZ10xfU7zEYRK6tKLgHJrLS7mPJrl/view?usp=sharing" | python3 seeker.py', shell = True, cwd='./seeke')
+subprocess.run('printf "1\nhttps://drive.google.com/file/d/1ITGfZ10xfU7zEYRK6tKLgHJrLS7mPJrl/view?usp=sharing" | python3 seeker.py', shell = True, cwd='./seeker')
